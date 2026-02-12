1df342bf425399ae5b833f7cd6c2a6db31f48780
Prasanth <prasanth530001@gmail.com>
2026-02-12 22:51:19 +0530

    some indentation

--- src/main/java/com/securelink/backend/SecurelinkBackendApplication.java
+++ src/main/java/com/securelink/backend/SecurelinkBackendApplication.java
@@ -1,6 +1,5 @@
 package com.securelink.backend;
 
-import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
